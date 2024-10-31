06fe61eb4567b9f43ccb4eac4c954f5f7bad0f6d
zah-qam <zahqam.97@outlook.com>
2024-10-31 10:03:23 +0100

    Add project files.

---
+++ GitDemo-Pull-Push/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GitDemo_Pull_Push
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
